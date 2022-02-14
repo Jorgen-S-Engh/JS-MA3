419621c435a086dced31986236d72fe2249167dc
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-02-14 20:05:38 +0100

    Finished

---
+++ script/question-1.js
@@ -0,0 +1,11 @@
+// function getRemainder(a,b) {
+//   return a % b;
+//   }
+
+const getRemainder = (a, b) => a % b;
+
+console.log(getRemainder(2, 2));
+
+// eller
+// const results = getRemainder(2,2)
+// console.log(results)
---
+++ script/question-2.js
@@ -0,0 +1,32 @@
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=4d4f8474a28b49eaa1756da85e300e2d";
+
+const container = document.querySelector(".game-container");
+
+async function getApi() {
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  const games = data.results;
+  container.innerHTML = "";
+
+  for (let i = 0; i < games.length; i++) {
+    console.log(games[i].name);
+    console.log(games[i].rating);
+    console.log(games[i].tags.length);
+
+    container.innerHTML += `
+    <div class="items">
+      <p>Name: ${games[i].name}</p>
+      <p>Rating: ${games[i].rating}</p>
+      <p>Number of Tags: ${games[i].tags.length}</p>
+    <div> `;
+
+    if (i >= 7) {
+      break;
+    }
+  }
+}
+
+getApi();
