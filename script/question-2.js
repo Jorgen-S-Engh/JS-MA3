1e25f16a60c397dff24d8bcb334344e0ec9f57e0
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-02-15 13:59:25 +0100

    Added try/catch

--- script/question-2.js
+++ script/question-2.js
@@ -4,29 +4,36 @@ const url =
 const container = document.querySelector(".game-container");
 
 async function getApi() {
-  const response = await fetch(url);
 
-  const data = await response.json();
+  try{
+    const response = await fetch(url);
 
-  const games = data.results;
-  container.innerHTML = "";
-
-  for (let i = 0; i < games.length; i++) {
-    console.log(games[i].name);
-    console.log(games[i].rating);
-    console.log(games[i].tags.length);
-
-    container.innerHTML += `
-    <div class="items">
-      <p>Name: ${games[i].name}</p>
-      <p>Rating: ${games[i].rating}</p>
-      <p>Number of Tags: ${games[i].tags.length}</p>
-    <div> `;
-
-    if (i >= 7) {
-      break;
+    const data = await response.json();
+  
+    const games = data.results;
+    container.innerHTML = "";
+  
+    for (let i = 0; i < games.length; i++) {
+      console.log(games[i].name);
+      console.log(games[i].rating);
+      console.log(games[i].tags.length);
+  
+      container.innerHTML += `
+      <div class="items">
+        <p>Name: ${games[i].name}</p>
+        <p>Rating: ${games[i].rating}</p>
+        <p>Number of Tags: ${games[i].tags.length}</p>
+      <div> `;
+  
+      if (i >= 7) {
+        break;
+      }
     }
+    
+  }catch(error){
+    container.innerHTML = ("The following error has occured: " +error);
   }
+
 }
 
 getApi();
